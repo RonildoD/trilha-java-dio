edda1782b669716641ec7cdcb02f416b8d002140
ronildoD <ronildodias.contact@gmail.com>
2026-03-07 00:53:05 -0300

    Desafio Proposto: Conta Bancária pelo  Termimal

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in).useLocale(Locale.US);
+		
+		int numero;
+		String agencia;
+		String nomeCliente;
+		double saldo;
+		
+		System.out.println("Digite seu nome completo ");
+		nomeCliente =sc.next();
+		System.out.println("Por favor, digite o número da Agência !");
+		agencia =sc.next();
+		System.out.println("Por favor, digite o número da Conta !");
+		numero =sc.nextInt();
+		System.out.println("Por favor, digite a valor para Depositar !");
+		saldo = sc.nextDouble();
+		
+		System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua "
+				+ "agência é %s, conta %d e seu saldo %.2f já está disponível para saque !",nomeCliente , agencia, numero, saldo);
+		
+		
+		
+		
+	}
+
+}
